d899c6b09416edaa847c5266122926c4fd94ea38
gcola <uss@macbook-pro-de-uss.home>
2017-12-11 18:10:33 +0100

    gco
    ajout test

--- 1_ini_functions.js
+++ 1_ini_functions.js
@@ -1,3 +1,4 @@
+/*test*/
 /*Source: https://www.youtube.com/watch?v=xWKq86PWG0o*/
 /*IDE: https://ethereum.github.io/browser-solidity/#version=soljson-v0.4.19+commit.c4cbbb05.js&optimize=true*/
 pragma solidity ^0.4.0;
