a71b0981ea58e8d81f57a7ea2b328e40f92a13b1
gcola <uss@macbook-pro-de-uss.home>
2017-12-11 19:27:33 +0100

    uss

--- 3_ini_heritage.js
+++ 3_ini_heritage.js
@@ -1,3 +1,4 @@
+/*test4*/
 /*Source: https://www.youtube.com/watch?v=kOBet0BPKzg*/
 /*IDE: https://ethereum.github.io/browser-solidity/#version=soljson-v0.4.19+commit.c4cbbb05.js&optimize=true*/
 pragma solidity ^0.4.0;
